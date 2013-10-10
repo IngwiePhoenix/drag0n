fd344e8266c84e8cf8ac4ab8e2e47f705805bff3
Ingwie Phoenix <ingwie2000@googlemail.com>
2013-10-10 11:19:12 +0200

    Updated design. Its just minor to display the app list
    To see the app list generated off the OS: Menu -> Packages -> Install.
    it'll show you a long list, or more, all apps installed on your mac :)

--- drag0n.app/Contents/Resources/views/site/list.php
+++ drag0n.app/Contents/Resources/views/site/list.php
@@ -1,5 +1,9 @@
-<?php #print_r($_SERVER);
-	$apps = SpycObject(Yii::app()->Apple->getAppsString());
-	echo "<pre>";
-	print_r($apps->Applications);
-	echo "</pre>";
+<?php
+$apps = SpycObject(Yii::app()->Apple->getAppsString());
+foreach($apps->Applications as $name => $app) { ?>
+<div class="row entry"><ul style="list-style:none;">
+	<li><b>Name</b>: <?=$name?></li>
+	<li><b>Version</b>: <?=$app->Version?></li>
+	<li><b>Location</b>: <?=$app->Location?></li>
+</ul></div>
+<?php } ?>
